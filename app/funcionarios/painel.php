a9d76beffe22339482d2bbfb6b630c0471f9d7b2
Nathan7k <nathan7kban@gmail.com>
2025-09-17 14:59:21 -0300

    correção css

--- app/funcionarios/painel.php
+++ app/funcionarios/painel.php
@@ -12,6 +12,8 @@ if (isset($_SESSION['perfil']) && $_SESSION['perfil'] === 'admin') {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../assets/css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <title>Painel do Administrador</title>
     </head>
 
